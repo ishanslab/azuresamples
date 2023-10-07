0238f3dc998fd18859509daf7eec85b111004692
ishanslab <90043870+ishanslab@users.noreply.github.com>
2023-10-07 16:21:30 +0530

    Update simpleaks.sh

--- azcliscripts/simpleaks.sh
+++ azcliscripts/simpleaks.sh
@@ -37,10 +37,7 @@ az group create --name $RG --location $LOC
 CURRENT_IP=$(dig @resolver1.opendns.com ANY myip.opendns.com +short)
 
 
-az aks create -g $RG -n $AKSNAME -l $LOC \
-  --node-count 3 \
-  --network-plugin azure \
-  --api-server-authorized-ip-ranges $CURRENT_IP/32 --generate-ssh-keys
+az aks create -g $RG -n $AKSNAME -l $LOC --node-count 3 --network-plugin azure --api-server-authorized-ip-ranges $CURRENT_IP/32 --generate-ssh-keys
  # --vnet-subnet-id $SUBNETID \
 
 
